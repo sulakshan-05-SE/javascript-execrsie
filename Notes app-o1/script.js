8dc31e81aa8371fc414321d9a07478823fbeac00
Sulakshan <msulaxan05@gmail.com>
2026-07-19 23:33:55 +0530

    delete function add

--- Notes app-o1/script.js
+++ Notes app-o1/script.js
@@ -1,1 +1,15 @@
-const notesCopntainer
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input_box");
+
+createBtn.addEventListener("click",()=>{
+    let inputBox = document.createElement("p");
+    let img = document.createElement9("img")
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable","true");
+    img.src ="images/delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+
+})
+
+no
